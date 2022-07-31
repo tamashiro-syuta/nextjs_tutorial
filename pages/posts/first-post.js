2ffb4bcef36c990aebaf620eba8fbb848e44560e
tamashiro-syuta <syuta5825@gmail.com>
2022-07-31 13:51:40 +0900

    Scriptタグにより、スクリプトの読み込みや実行を最適化

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,23 @@
 import Link from "next/link";
 import Head from "next/head";
+import Script from "next/script";
 
 const FirstPost = () => {
   return (
     <>
-      {/* Headタグでペ時のメタデータを設定できる */}
+      {/* Headタグでページのメタデータを設定できる */}
       <Head>
         <title>FirstPost</title>
       </Head>
+      {/* Scriptタグで、スクリプトの読み込みや実行を最適化 => 通常のscriptタグではsrcのフェッチにより他コンテンツの読み込みが遅れる可能性がある */}
+      {/* スクリプトのレイジーロードを行うようにする => レージーロードとは、表示範囲外のコンテンツの読み込みを遅延させ、画面表示の高速化を図ること */}
+      <Script
+        src="https://connect.facebook.net/en_US/sdk.js"
+        strategy="lazyOnLoad"
+        onLoad={() =>
+          console.log(`script loaded correctly, window.FB has been populated`)
+        }
+      />
       <h1>First Post</h1>
       <h2>
         <Link href={"/"}>
