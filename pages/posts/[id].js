8391303ba29dfe2149fcde7a8b57583cde463bf4
tamashiro-syuta <syuta5825@gmail.com>
2022-08-06 10:48:55 +0900

    動的ルーティング

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,34 @@
-import { getAllPostIds } from "../../lib/posts";
+// [hoge].js のページよりリクエストされたURLに応じて動的なページが返されるようになる
 
-export default getStaticPaths = async () => {
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+
+export const getStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
     fallback: false,
   };
 };
+
+export const getStaticProps = async ({ params }) => {
+  const postData = getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+};
+const Post = ({ postData }) => {
+  return (
+    <Layout>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+    </Layout>
+  );
+};
+
+export default Post;
